147c1a1867905073e7c1599d65aab4d16195e1c2
neekeshz <nikes594@gmail.com>
2024-05-30 19:38:06 +0545

    Initial commit

---
+++ shop/cart_models.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db import models
+from shop.models import Product
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='cart_items', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
---
+++ shop/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+from .cart_models import Cart, CartItem
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    
+    return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
+
+def add_to_cart(request, product_id):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('shop:product_detail', id=product.id, slug=product.slug)
+
+def cart_detail(request):
+    cart = get_object_or_404(Cart, user=request.user)
+    return render(request, 'shop/cart/detail.html', {'cart': cart})
